12d7a771939fa75d9d5e436feb6e6b3d227a6ab5
juan1santiusty <juan@Debian.Juan>
2025-05-20 17:46:38 -0500

    Funciones auxiliares

--- src/03_kernel_base/kernel/src/kernel.c
+++ src/03_kernel_base/kernel/src/kernel.c
@@ -19,6 +19,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Variable global para estado del teclado
+int estado_ctrl=0;
+//Mapa de ctrl
+char mapa_ctrl[]={0,0};
+
 void cmain(){
 
     /* Inicializar y limpiar la consola console.c */
@@ -29,6 +34,8 @@ void cmain(){
 
     /* Las subrutinas que se deben ejecutar ANTES de habilitar las
      * interrupciones se deben invicar en este punto */
+     
+     
 
     /* Configura la IDT y el PIC.interrupt.c */
     setup_interrupts();
@@ -48,4 +55,45 @@ void cmain(){
     /* Imprimir un mensaje por pantalla. */
     console_printf("Kernel started.\n");
 
+}
+
+//Función para procesar teclas modificadas
+void procesar_modificadoras(unsigned char code){
+  switch(code){
+    case 0x1D:  //Ctrl make
+      estado_ctrl = 1;
+      break;
+    case 0x9D:  //Ctrl break
+      estado_ctrl = 0;
+      break;
+  }
+}
+
+//Función para saber si algo es imprimible
+int es_imprimible(unsigned char code) {
+  if((code >= 0x02 && code <= 0x0D) ||
+    (code >= 0x10 && code <= 0x1C) ||
+    (code >= 0x1E && code <= 0x26) ||
+    (code >= 0x2C && code <= 0x32) ||
+    (code == 0x39 )
+    ){
+    return 1;
+  }
+  return 0;
+}
+
+//Función para traducir ScanCode
+unsigned char traducir(unsigned char code){
+     static const unsigned char ascii_table[] = {
+        0,  0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,
+        0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+        0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+        0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*',
+        0, ' ', 0, 0
+    };
+    
+     if (code >= sizeof(ascii_table)/sizeof(ascii_table[0])) {
+        return 0;
+    }
+        return ascii_table[code];
 }
